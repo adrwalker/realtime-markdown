95bcfc8f23fab31b3f7bf0a214150254fa470f4a
Adrian Walker <adrwalker@gmail.com>
2016-01-02 16:47:03 -0500

    Finalizing markdown application

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('pad');
 });
+app.get('/(:id)', function(req,res) {
+	res.render('pad');
+});
+
+// get ShareJS dependencies
+var sharejs = require('share');
+require('redis');
+
+// options for ShareJS
+var options = {
+	db: {type: 'redis'}
+};
+
+// Attach the express server to ShareJS
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
